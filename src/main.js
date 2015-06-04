523e73d8b8ffdcd2c837c048eeefcaf621546abb
Ryan Jones <ryan.joshua.jones@gmail.com>
2015-06-04 13:15:27 -0700

    scene + lighting + render

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import init from './init';
+import setupScene from './scene';
+import animate from './animate';
 
 const { scene, camera, renderer, controls } = init();
+setupScene(scene);
+animate(scene, camera, renderer);
+
+
