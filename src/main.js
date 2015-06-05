61a951dc1c12b415b31cf16a75924144395e8594
Ryan Jones <ryan.joshua.jones@gmail.com>
2015-06-05 10:26:11 -0700

    add room stuff

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import setupTweening from './tweening';
 import animate from './animate';
 import createPictures from './create_pictures';
 import createHUD from './create_hud';
+import setupRoom from './setup_room';
 
 const { scene, objects, camera, renderer, controls, clock } = init();
 const HUD = createHUD();
 setupScene(scene, objects);
 createPictures(scene, objects);
+setupRoom(scene);
 //setupTweening(objects.basecube);
 animate(scene, objects, camera, renderer, controls, clock, HUD);
 
