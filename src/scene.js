dde2448810a6091b80ba5c797ad23cb152b3a43a
Dan Pastusek <dan@imgur.com>
2015-06-05 00:59:54 -0700

    yay! perspective.

--- src/scene.js
+++ src/scene.js
@@ -5,34 +5,20 @@ export default function(scene, objects) {
   const MAP_WIDTH = 20;
   THREE.ImageUtils.crossOrigin = '';
 
-  let imageUrl = 'http://i.imgur.com/xhTUQwZ.gif';
-
+  let imageUrl = 'http://i.imgur.com/riembhY.jpg'; // img/grid.jpg doesn't work due to cross origin crap
   var texture = THREE.ImageUtils.loadTexture( imageUrl );
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 1, 2 );
-
-  let blankMaterial = new THREE.MeshPhongMaterial({ color: 0xFFFFFF });
-  let material = new THREE.MeshPhongMaterial({
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.set( 10, 10 );
+  let material = new THREE.MeshBasicMaterial({
     map: texture,
-    color: 0xFFFFFF
+    side: THREE.BackSide
   });
-
-  let materials = [
-    blankMaterial,
-    blankMaterial,
-    material,
-    blankMaterial,
-    blankMaterial,
-    blankMaterial
-  ];
-
   const floor = new THREE.Mesh(
-    new THREE.BoxGeometry(MAP_WIDTH * UNIT_SIZE, 10, MAP_WIDTH * UNIT_SIZE),
-    new THREE.MeshFaceMaterial(materials)
+    new THREE.PlaneGeometry(1000,1000),
+      material
   );
-
-  floor.position.set(0, -10, 0);
+  floor.position.set(0, 0, 0);
+  floor.rotation.x = Math.PI / 2;
   scene.add(floor);
   objects.floor = floor;
 
